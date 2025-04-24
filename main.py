57194505f1179ffc68893eec273bc03ede6e8798
kaithomsonbksb <30250030@blackpool.ac.uk>
2025-04-24 10:33:13 +0100

    added typing functionality

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Calculator:
         self.screen = tk.Entry(root, font=("Arial", 20), bd=10, insertwidth=2, width=14, borderwidth=4, justify="right")
         self.screen.grid(row=0, column=0, columnspan=4, pady=20)
 
+        # Bind keyboard input
+        self.root.bind("<Key>", self.on_key_press)
+
         # Buttons
         self.create_buttons()
 
@@ -42,6 +45,19 @@ class Calculator:
                 self.screen.insert(0, "Error")
         else:
             self.screen.insert(tk.END, char)
+
+    def on_key_press(self, event):
+        key = event.char
+        if key in "0123456789+-*/^":
+            self.on_button_click(key)
+        elif key == "\r":  # Enter key
+            self.on_button_click("=")
+        elif key == "\x08":  # Backspace key
+            current_text = self.screen.get()
+            self.screen.delete(0, tk.END)
+            self.screen.insert(0, current_text[:-1])
+        elif key.lower() == "c":  # Clear screen with 'C' or 'c'
+            self.on_button_click("C")
 
 
 root = tk.Tk()
